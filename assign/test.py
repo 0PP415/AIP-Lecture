3be0855398567b7c32c5d30a0a77f88f09c118e2
0PP415 <majw0415@gmail.com>
2025-10-13 22:10:37 +0900

    Update test.py

--- assign/test.py
+++ assign/test.py
@@ -1,11 +1,34 @@
-var = 1
-print(id(var))
+def wow(target):
+    print(id(target))
 
-var = 2
-print(id(var))
+class Student:
+    def __init__(self, name, id, dept):
+        self.__name = name
+        self._id = id
+        self._dept = dept
 
-var1 = [1, 2, 3]
-print(id(var1))
+    def getName(self):
+        return self.__name
 
-var1 = [1, 2]
-print(id(var1))
+    def setName(self, name):
+        self.__name = name
+
+    def printStudentInfo(self):
+        print(self.__name, self._id, self._dept)
+
+stu = [Student("Tom", 112234, "CSE"),
+       Student("Cindy", 123456, "IE"),
+       Student("Chris", 124578, "EE")]
+
+# print(stu[0].__name)
+print(stu[0]._Student__name)
+
+
+my = Student("tmp", 0, "")
+Student.__init__(my, "my", 2024, "CCC")
+print(Student.getName(my))
+
+stu.append(my)
+
+for i in range(len(stu)):
+    stu[i].printStudentInfo()
