d07f2a05de4325d1070efb9e4fe6ea9886e5813c
0PP415 <majw0415@gmail.com>
2025-11-11 16:52:27 +0900

    Update HW7

--- SA_assign_v0/first-choice (tsp).py
+++ SA_assign_v0/first-choice (tsp).py
@@ -1,9 +1,9 @@
 import random
 import math
 
-LIMIT_STUCK = 100 # Max number of evaluations enduring no improvement
-NumEval = 0    # Total number of evaluations
+from tsp import *
 
+LIMIT_STUCK = 100 # Max number of evaluations enduring no improvement
 
 def main():
     # Create an instance of TSP
@@ -15,36 +15,6 @@ def main():
     displaySetting()
     # Report results
     displayResult(solution, minimum)
-    
-def createProblem():
-    ## Read in a TSP (# of cities, locatioins) from a file.
-    ## Then, create a problem instance and return it.
-    fileName = input("Enter the file name of a TSP: ")
-    infile = open(fileName, 'r')
-
-    # First line is number of cities
-    numCities = int(infile.readline())
-    locations = []
-
-    line = infile.readline()  # The rest of the lines are locations
-    while line != '':
-        locations.append(eval(line)) # Make a tuple and append
-        line = infile.readline()
-    infile.close()
-    
-    table = calcDistanceTable(numCities, locations)
-    return numCities, locations, table
-
-
-def calcDistanceTable(numCities, locations): ###
-    table = [[0.0] * numCities for _ in range(numCities)]
-
-    for start in range(numCities):
-        for dest in range(numCities):
-            if start != dest:
-                table[start][dest] = math.sqrt((locations[start][0] - locations[dest][0])**2 + (locations[start][1] - locations[dest][1])**2)
-
-    return table # A symmetric matrix of pairwise distances
 
 
 def firstChoice(p):
@@ -62,36 +32,6 @@ def firstChoice(p):
             i += 1
     return current, valueC
 
-def randomInit(p):   # Return a random initial tour
-    n = p[0]
-    init = list(range(n))
-    random.shuffle(init)
-    return init
-
-
-def evaluate(current, p): ###
-    ## Calculate the tour cost of 'current'
-    ## 'p' is a Problem instance
-    ## 'current' is a list of city ids
-    global NumEval
-    NumEval += 1
-
-    cost = 0.0
-
-    numCities = p[0]
-    table = p[2]
-
-    for i in range(numCities - 1):
-        curr = current[i]
-        next = current[i+1]
-        cost += table[curr][next]
-
-    first = current[-1]
-    last  = current[0]
-    cost += table[first][last]
-
-    return cost
-
 
 def randomMutant(current, p): # Apply inversion
     while True:
@@ -102,42 +42,10 @@ def randomMutant(current, p): # Apply inversion
             break
     return curCopy
 
-def inversion(current, i, j):  # Perform inversion
-    curCopy = current[:]
-    while i < j:
-        curCopy[i], curCopy[j] = curCopy[j], curCopy[i]
-        i += 1
-        j -= 1
-    return curCopy
-
-
-def describeProblem(p):
-    print()
-    n = p[0]
-    print("Number of cities:", n)
-    print("City locations:")
-    locations = p[1]
-    for i in range(n):
-        print("{0:>12}".format(str(locations[i])), end = '')
-        if i % 5 == 4:
-            print()
 
 def displaySetting():
     print()
     print("Search algorithm: First-Choice Hill Climbing")
 
-def displayResult(solution, minimum):
-    print()
-    print("Best order of visits:")
-    tenPerRow(solution)       # Print 10 cities per row
-    print("Minimum tour cost: {0:,}".format(round(minimum)))
-    print()
-    print("Total number of evaluations: {0:,}".format(NumEval))
-
-def tenPerRow(solution):
-    for i in range(len(solution)):
-        print("{0:>5}".format(solution[i]), end='')
-        if i % 10 == 9:
-            print()
 
 main()
--- SA_assign_v0/steepest ascent (tsp).py
+++ SA_assign_v0/steepest ascent (tsp).py
@@ -1,8 +1,9 @@
 import random
 import math
 
-NumEval = 0    # Total number of evaluations
+from tsp import *
 
+LIMIT_STUCK = 100 # Max number of evaluations enduring no improvement
 
 def main():
     # Create an instance of TSP
@@ -14,36 +15,6 @@ def main():
     displaySetting()
     # Report results
     displayResult(solution, minimum)
-    
-def createProblem():
-    ## Read in a TSP (# of cities, locatioins) from a file.
-    ## Then, create a problem instance and return it.
-    fileName = input("Enter the file name of a TSP: ")
-    infile = open(fileName, 'r')
-
-    # First line is number of cities
-    numCities = int(infile.readline())
-    locations = []
-
-    line = infile.readline()  # The rest of the lines are locations
-    while line != '':
-        locations.append(eval(line)) # Make a tuple and append
-        line = infile.readline()
-    infile.close()
-
-    table = calcDistanceTable(numCities, locations)
-    return numCities, locations, table
-
-
-def calcDistanceTable(numCities, locations): ###
-    table = [[0.0] * numCities for _ in range(numCities)]
-
-    for start in range(numCities):
-        for dest in range(numCities):
-            if start != dest:
-                table[start][dest] = math.sqrt((locations[start][0] - locations[dest][0])**2 + (locations[start][1] - locations[dest][1])**2)
-
-    return table # A symmetric matrix of pairwise distances
 
 
 def steepestAscent(p):
@@ -58,36 +29,6 @@ def steepestAscent(p):
             current = successor
             valueC = valueS
     return current, valueC
-
-def randomInit(p):   # Return a random initial tour
-    n = p[0]
-    init = list(range(n))
-    random.shuffle(init)
-    return init
-
-
-def evaluate(current, p): ###
-    ## Calculate the tour cost of 'current'
-    ## 'p' is a Problem instance
-    ## 'current' is a list of city ids
-    global NumEval
-    NumEval += 1
-
-    cost = 0.0
-
-    numCities = p[0]
-    table = p[2]
-
-    for i in range(numCities - 1):
-        curr = current[i]
-        next = current[i+1]
-        cost += table[curr][next]
-
-    first = current[-1]
-    last  = current[0]
-    cost += table[first][last]
-
-    return cost
 
 
 def mutants(current, p): # Apply inversion
@@ -104,13 +45,6 @@ def mutants(current, p): # Apply inversion
             neighbors.append(curCopy)
     return neighbors
 
-def inversion(current, i, j):  ## Perform inversion
-    curCopy = current[:]
-    while i < j:
-        curCopy[i], curCopy[j] = curCopy[j], curCopy[i]
-        i += 1
-        j -= 1
-    return curCopy
 
 def bestOf(neighbors, p): ###
     best = neighbors[0]
@@ -124,33 +58,10 @@ def bestOf(neighbors, p): ###
 
     return best, bestValue
 
-def describeProblem(p):
-    print()
-    n = p[0]
-    print("Number of cities:", n)
-    print("City locations:")
-    locations = p[1]
-    for i in range(n):
-        print("{0:>12}".format(str(locations[i])), end = '')
-        if i % 5 == 4:
-            print()
 
 def displaySetting():
     print()
     print("Search algorithm: Steepest-Ascent Hill Climbing")
 
-def displayResult(solution, minimum):
-    print()
-    print("Best order of visits:")
-    tenPerRow(solution)       # Print 10 cities per row
-    print("Minimum tour cost: {0:,}".format(round(minimum)))
-    print()
-    print("Total number of evaluations: {0:,}".format(NumEval))
-
-def tenPerRow(solution):
-    for i in range(len(solution)):
-        print("{0:>5}".format(solution[i]), end='')
-        if i % 10 == 9:
-            print()
 
 main()
